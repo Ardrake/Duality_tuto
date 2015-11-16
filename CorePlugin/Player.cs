8724afe3f2a7f128a9f9a9fddae285c529789f4b
André Cooke <andrecooke@hotmail.com>
2015-11-16 12:43:07 -0500

    Initial commit

---
+++ CorePlugin/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+using Duality.Components.Physics;
+using Duality.Input;
+
+namespace YourFirstProject
+{
+    [RequiredComponent(typeof(RigidBody))]
+    public class Player : Component, ICmpUpdatable
+    {
+        void ICmpUpdatable.OnUpdate()
+        {
+            RigidBody body = this.GameObj.GetComponent<RigidBody>();
+
+            if (DualityApp.Keyboard[Key.A])
+                body.ApplyLocalForce(-0.001f);
+            else if (DualityApp.Keyboard[Key.D])
+                body.ApplyLocalForce(0.001f);
+            else
+                body.AngularVelocity -= body.AngularVelocity * 0.1f * Time.TimeMult;
+
+            if (DualityApp.Keyboard[Key.W])
+                body.ApplyLocalForce(Vector2.UnitY * -0.2f * body.Mass);
+            else if (DualityApp.Keyboard[Key.S])
+                body.ApplyLocalForce(Vector2.UnitY * 0.2f * body.Mass);
+        }
+    }
+}
